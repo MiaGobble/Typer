fba22cdc9c3926ea731107978e985e5f3bd65663
miagobble <miagobble@duck.com>
2026-01-18 10:47:09 -0500

    Built and published by Moonwave

---
+++ assets/js/41e338b2.e5507a35.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[359],{4410:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/MiaGobble/Typer/blob/main","baseUrl":"/Typer/","classOrder":[],"apiCategories":[]}')}}]);
